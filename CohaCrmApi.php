36153307d607c6fd0dc1ae2fe16c825818a45cd9
abachschmid <>
2020-08-06 21:02:09 +0200

    Logs extended. API-Connection in Shopware Settings

--- CohaCrmApi.php
+++ CohaCrmApi.php
@@ -29,10 +29,19 @@ class CohaCrmApi extends Plugin
         $request = $subject->Request();
         $params = $request->getParams();
 
-        $this->apiCallCrm($params);
+        $this->buildApiCall($params);
     }
 
-    public function apiCallCrm($params) {
+    public function logToFile($filename, $content, $timestamp) {
+        // Build File-Content
+        $filepath = __DIR__ . DIRECTORY_SEPARATOR . 'Logs' . DIRECTORY_SEPARATOR . $filename;
+        $filecontent = "[{$timestamp}]\r\n{$content}\r\n\r\n";
+
+        // Put into File
+        file_put_contents($filepath, $filecontent, FILE_APPEND);
+    }
+
+    public function buildApiCall($params) {
         /*
             curl -v -H "Accept: application/json" -H "Content-type: application/json" 
             -X POST -d '{"person":{"first_name":"Marian","name":"Miller"}}'  
@@ -43,27 +52,65 @@ class CohaCrmApi extends Plugin
             "updated_by_user_id":null,
             "created_at":"2015-07-20T13:26:42.190Z","updated_at":"2015-07-20T13:26:42.190Z"}}
         */
+        
+        // Get Date
+        $now = new \DateTime();
+        $timestamp = $now->getTimestamp();
 
-        file_put_contents(
-            'test.json',
-            json_encode(
-                $params
-            )
-        );
+        // Start Log to File
+        $this->logToFile('shopware-params.log', json_encode($params), $timestamp);
 
-        $api_url = 'XXX_DIE URL AUS CONFIG: https://api.centralstationcrm.net/api/people.json';
-        $api_key = '582398275....DERKEYAUSKEEPASS!';
+        
+        // Generate Api Data
+        $postfields = $this->generateParameters($params);
+        $postfields['apikey'] = $this->getConfig('apiKey');
+        $api_url = $this->getConfig('apiDomain') . '/api/people.json';
 
+        // Fire API
+        $this->fireApiCall($api_url, $postfields, $timestamp);
+    }
 
-        $postfields = json_encode([
-            'apikey' => $api_key,
+    public function fireApiCall($url, $params, $timestamp)
+    {
+        $cURLConnection = curl_init();
+
+        curl_setopt($cURLConnection, CURLOPT_POST, true);
+        curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, ['Content-Type: application/json; charset=utf-8']);
+        curl_setopt($cURLConnection, CURLOPT_URL, $url);
+        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURLConnection, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($cURLConnection, CURLOPT_SSL_VERIFYHOST, 0);
+        
+        $response = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+
+        // Log to File
+        $this->logToFile('crm-response.log', $response, $timestamp);
+
+        // Maybe hier response code. wenn ungleich 200 dann log + email an it@ raus!
+        // HTTP-Status-Code prüfen
+        if (!curl_errno($cURLConnection)) {
+            switch ($http_code = curl_getinfo($cURLConnection, CURLINFO_HTTP_CODE)) {
+                case 200:
+                    // Alles Ok!
+                    break;
+                default:
+                    // echo 'Unerwarter HTTP-Code: ', $http_code, "\n";
+                    break;
+            }
+        }
+    }
+
+    public function generateParameters($params) {
+        return [
             'person' => [
                 'title' => $params['anrede'] ?? '',
                 'first_name' => $params['vorname'] ?? '',
                 'name' => $params['nachname']  ?? '',
                 'email' => $params['email'] ?? '',
                 'company' => $params['unternehmen'] ?? '',
-                'background' => $params['betreff'] . $params['kommentar'],
+                'background' => ($params['betreff'] ?? '') . ' - ' . ($params['kommentar'] ?? ''),
 
                 'positions_attributes' => [
                     [
@@ -77,30 +124,18 @@ class CohaCrmApi extends Plugin
                     [
                         'id' => '',
                         'name' => 'Online-Shop Kontaktformular'
+                    ],
+                    [
+                        'id' => '',
+                        'name' => 'form-id-' . ($params['id'] ?? '')
                     ]
                 ]
             ]
-        ]);
+        ];
+    }
 
-
-        $cURLConnection = curl_init();
-
-        curl_setopt($cURLConnection, CURLOPT_POST, true);
-        curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, ['Content-Type: application/json; charset=utf-8']);
-        curl_setopt($cURLConnection, CURLOPT_URL, $api_url);
-        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postfields);
-        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($cURLConnection, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($cURLConnection, CURLOPT_SSL_VERIFYHOST, 0);
-        
-        $response = curl_exec($cURLConnection);
-        curl_close($cURLConnection);
-
-        // Maybe hier response code. wenn ungleich 200 dann log + email an it@ raus!
-
-        // DEBUG TMP - löschen sobald live
-        file_put_contents('test.json', $response, FILE_APPEND);
-
+    public function getConfig($key) {
+        return Shopware()->Config()->getByNamespace('CohaCrmApi', $key);
     }
 
 }
